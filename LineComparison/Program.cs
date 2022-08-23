11a74f18069520cd2506df60ffc1e63bf2d059f1
progwruk <wmeshram@gmail.com>
2022-08-23 17:30:08 +0530

    [Wrukshali] Add: Check equality

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,7 +7,11 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Comparision comparision = new Comparision(1, 2, 3, 4);
-            comparision.CalculateLength();
+            double line1 = comparision.CalculateLength();
+            Console.WriteLine("Length of first line "+line1);
+            Comparision LineComparision = new Comparision(1, 2, 3, 4);
+            double line2 = LineComparision.CalculateLength();
+            Console.WriteLine("Length of second line "+line2);
         }
     }
 }
